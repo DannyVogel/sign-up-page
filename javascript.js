c4eb3e3f67a09d2bf9b4d37600d3dbdf97282145
DannyVogel <danny.hj.vogel@gmail.com>
2023-01-19 16:59:47 +0100

    implement form features; password verification & confirmation text

--- javascript.js
+++ javascript.js
@@ -7,3 +7,24 @@ function toggleDarkMode() {
         ? background.setAttribute('src', './images/moon.jpg')
         : background.setAttribute('src', './images/sun.jpg')
 }
+
+function processForm(event) {
+    const name = document.getElementById("firstName").value
+    
+    const password = document.getElementById("password")
+    const confirmPassword = document.getElementById("confirmPassword")
+    console.log("?", password, confirmPassword)
+
+    if (password.value !== confirmPassword.value){
+        password.style.border = "2px solid red"
+        confirmPassword.style.border = "2px solid red"
+                
+        alert("Passwords do not match.")
+        event.preventDefault()
+        return false;
+    } else {
+        document.querySelector(".finalP").innerText = `Thanks for signing up ${name}`
+        event.preventDefault()
+        return false;
+    }
+}
